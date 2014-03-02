d752356f2765e033d4d3ad9621c38f2a2e1c71b8
Julian Browne <julianbrowne@gmail.com>
2014-03-02 15:01:13 +0000

    tidy up

--- docket.js
+++ docket.js
@@ -16,34 +16,39 @@ console.log('Waiting for browser to connect at http://127.0.0.1:' + port);
 conn.sockets.on('connection', function (socket) { 
 
     console.log('Browser connected');
+    var shop = new docket.Pipe(socket);
+    shop.listen("sales", "new");
 
-    var pipeline = new docket.Pipe(socket);
-    pipeline.listen("sales", "new");
+    socket.on('shop', function(data) { 
 
-    pipeline.sold(1, "bottles", "olive oil");
-    pipeline.sold(2, "loaves", "bread");
-    pipeline.sold(1, "packs", "butter");
-    pipeline.sold(1, "litres", "fabric conditioner");
-    pipeline.sold(3, "grams", "cheese");
-    pipeline.sold(1, "boxes", "crackers");
-    pipeline.sold(1, "jars", "coffee");
-    pipeline.sold(2, "lbs", "rice");
-    pipeline.sold(2, "bottles", "port");
-    pipeline.sold(3, "packets", "mince pies");
-    pipeline.sold(1, "whole", "chicken");
-    pipeline.sold(2, "bottles", "milk");
-    pipeline.sold(1, "cans", "air freshener");
-    pipeline.sold(2, "packets", "twiglets");
-    pipeline.sold(5, "lbs", "potatoes");
-    pipeline.sold(4, "boxes", "chocolate orange");
-    pipeline.sold(1, "bottles", "washing-up liquid");
-    pipeline.sold(1, "whole", "goose");
-    pipeline.sold(1, "bottles", "mulled wine");
-    pipeline.sold(1, "lbs", "brocolli");
-    pipeline.sold(2, "lbs", "sugar");
-    pipeline.sold(4, "lbs", "sprouts");
-    pipeline.sold(3, "packets", "kitchen roll");
-    pipeline.sold(1, "lbs", "tea");
-    pipeline.sold(4, "cans", "dog food");
+        console.log('Starting to shop');
+
+        shop.sold(1, "bottles", "olive oil");
+        shop.sold(2, "loaves", "bread");
+        shop.sold(1, "packs", "butter");
+        shop.sold(1, "litres", "fabric conditioner");
+        shop.sold(3, "grams", "cheese");
+        shop.sold(1, "boxes", "crackers");
+        shop.sold(1, "jars", "coffee");
+        shop.sold(2, "lbs", "rice");
+        shop.sold(2, "bottles", "port");
+        shop.sold(3, "packets", "mince pies");
+        shop.sold(1, "whole", "chicken");
+        shop.sold(2, "bottles", "milk");
+        shop.sold(1, "cans", "air freshener");
+        shop.sold(2, "packets", "twiglets");
+        shop.sold(5, "lbs", "potatoes");
+        shop.sold(4, "boxes", "chocolate orange");
+        shop.sold(1, "bottles", "washing-up liquid");
+        shop.sold(1, "whole", "goose");
+        shop.sold(1, "bottles", "mulled wine");
+        shop.sold(1, "lbs", "brocolli");
+        shop.sold(2, "lbs", "sugar");
+        shop.sold(4, "lbs", "sprouts");
+        shop.sold(3, "packets", "kitchen roll");
+        shop.sold(1, "lbs", "tea");
+        shop.sold(4, "cans", "dog food");
+
+    });
 
 });
